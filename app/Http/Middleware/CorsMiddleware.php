427f504ca9fb45d941a41284db144b4e6df7e7eb
@GA17033-prn335-2018 <alexis.garcia170698@gmail.com>
2023-06-12 13:20:44 -0600

    cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -6,15 +6,25 @@ use Closure;
 
 class CorsMiddleware
 {
-    public function handle($request, Closure $next)
+    public function handle($request, \Closure $next)
     {
+        $headers = [
+            'Access-Control-Allow-Origin'      => '*',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, Recaptcha'
+        ];
+
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
+
         $response = $next($request);
-        
-        // Configurar las cabeceras CORS
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+
         return $response;
     }
 }
